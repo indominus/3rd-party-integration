6ab63ae59cbdf80a5b3990c56306a651bc25a85b
Zlatin Hristov <zlatin.hristov@devop.studio>
2021-03-03 21:27:30 +0200

    New workflow test
    Signed-off-by: Zlatin Hristov <zlatin.hristov@devop.studio>

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
